da2d2f628b3d172ac8ce319a02493a2c72576e93
tt2t2am1118 <tt2t2am1118@naver.com>
2023-08-06 07:31:57 +0900

    first commit

---
+++ app/src/main/java/gptcode/SwingUI.java
@@ -0,0 +1,157 @@
+package gptcode;
+import javax.swing.*;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import gptcode.themes.MyFlatLaf;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.*;
+ 
+
+public class SwingUI extends JFrame {
+    private JTextField textField;
+    private JButton uploadButton;
+    private JButton sendButton;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
+
+    private List<String> systemStr = new ArrayList<>();
+    private List<String> userStr = new ArrayList<>();
+
+    private static final String API_KEY = "OpenAI API KEY";
+    private OkHttpClient client;
+
+    public SwingUI() {
+        setTitle("Swing ChatGpt");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        textField = new JTextField(20);
+        uploadButton = new JButton("File");
+        sendButton = new JButton("Enter");
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        topPanel.add(textField, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        topPanel.add(uploadButton, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        topPanel.add(sendButton, gbc);
+
+        textArea = new JTextArea(30, 50);
+        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        textArea.setLineWrap(true);
+        add(topPanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String response = sendQuestionToOpenAI(textField.getText());
+                    textArea.setText(response);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+
+        //엔터키를 입력하면, 자동으로 문장이 입력되도록 하는 부분.
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendButton.doClick();
+            }
+        });
+
+
+        setBounds(100, 100, 450, 300);
+        setVisible(true);
+    }
+
+    public String sendQuestionToOpenAI(String question) throws IOException, NullPointerException {
+         client = new OkHttpClient.Builder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .writeTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
+    .build();
+        
+        if(systemStr ==null || systemStr.size()==0){
+            systemStr.add("You are a helpful assistant.");
+        }
+        userStr.add(question);
+        
+        StringBuilder messages = new StringBuilder();
+        for(int i=0; i<systemStr.size(); i++){
+            messages.append("{\"role\": \"system\", \"content\": \""+systemStr.get(i)+"\"},");
+            messages.append("{\"role\": \"user\", \"content\": \""+userStr.get(i)+"\"},");
+            System.out.println(systemStr.get(i)+" "+ i+ " \n");
+            System.out.println(userStr.get(i)+" "+ i + "\n");
+        }
+        // 마지막 쉼표 제거
+        if (messages.length() > 0) {
+            messages.deleteCharAt(messages.length() - 1);
+        }
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        String json = "{"
+                + "\"model\": \"gpt-3.5-turbo\","
+                + "\"messages\": ["
+                + messages.toString()
+                + "]"
+                + "}";
+
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + API_KEY)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String responseBody = response.body().string();
+          
+              JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(responseBody);
+            JsonObject obj = element.getAsJsonObject();
+            
+            JsonArray choices = obj.getAsJsonArray("choices");
+            JsonObject choice = choices.get(0).getAsJsonObject();
+            JsonObject message = choice.getAsJsonObject("message");
+            String content = message.get("content").getAsString();
+            systemStr.add(content);
+            return content+"\n";
+        }
+       
+    }
+
+    public static void main(String[] args) {
+        MyFlatLaf.setup();
+        new SwingUI();
+    }
+}
---
+++ app/src/main/java/gptcode/themes/MyFlatLaf.java
@@ -0,0 +1,17 @@
+package gptcode.themes;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
+public class MyFlatLaf extends FlatDarkLaf {
+    public static boolean setup(){
+        return setup(new MyFlatLaf());
+    }
+
+    @Override
+    public String getName() {
+        return "My FlatLaf";
+    }
+
+
+}
